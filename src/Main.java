925246fa6816f9ceac1ea85fcc7ed5929d679160
Noahsharon8 <127nsharon@frhsd.com>
2025-09-08 10:35:16 -0400

    Response name statement

--- src/Main.java
+++ src/Main.java
@@ -3,5 +3,7 @@
 public class Main {
     public static void main(String[] args) {
         System.out.println("Hi, My name is Dylan Reger.");
+        System.out.println("Hi Dylan, I am Noah");
     }
+
 }
